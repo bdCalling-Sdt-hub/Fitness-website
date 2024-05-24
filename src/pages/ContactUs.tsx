a601e3eca09f4309237cc476e388a26d7fb03b21
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-05-24 15:42:41 +0600

    contact page responsive

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -13,7 +13,7 @@ const ContactUs = ():React.JSX.Element => {
             <Heading title='Contact Us'  style='mb-6' />
             <MetaTag title='Contact Us' />
 
-            <div className='flex items-center justify-between w-[572px] mx-auto'>
+            <div className='flex items-center flex-wrap justify-between max-w-[572px] mx-auto'>
                 <div className='flex gap-3'>
                     <div className='flex items-center gap-4 h-fit'>
                         <GoMail size={20} color='#575757' />
@@ -37,10 +37,10 @@ const ContactUs = ():React.JSX.Element => {
                 </div>
             </div>
 
-            <h1 className='text-[40px] leading-8 text-secondary text-center mt-16 font-normal'>Get in Touch</h1>
+            <h1 className='text-xl md:text-2xl lg:text-[40px] leading-8 text-secondary text-center mt-16 font-normal'>Get in Touch</h1>
             <p className='my-10 text-[18px] leading-[14px] text-secondary text-center font-normal'>Contact with us</p>
 
-            <Form layout='vertical' className='w-[572px] mx-auto'>
+            <Form layout='vertical' className='mx-w-[572px] mx-auto'>
                 <Form.Item 
                     name="email"
                     label={<p className="text-[#919191] text-[16px] leading-5 font-normal">Email</p>}
