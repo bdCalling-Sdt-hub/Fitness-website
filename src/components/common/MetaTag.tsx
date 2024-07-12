744046e109e50a571cdb4495beaf8b087e03bfa9
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-07-12 09:49:28 +0600

    fix issue

--- src/components/common/MetaTag.tsx
+++ src/components/common/MetaTag.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 const MetaTag = ({title} : {title: string} ):React.JSX.Element => {
     return (
         <Helmet>
-            <title> {`Unity & Motion ${ title ? "-" : "" } ${title}`}</title>
+            <title> {`Unity In Motion ${ title ? "-" : "" } ${title}`}</title>
         </Helmet>
     )
 }
